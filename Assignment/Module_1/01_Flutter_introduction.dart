ca5f0272b4693d985b071e4f25952090dd7d4840
bhavyabarot <bhavyabarot940@gmail.com>
2023-03-20 18:23:39 +0530

    complited
    complited

---
+++ Assignment/Module_1/01_Flutter_introduction.dart
@@ -0,0 +1,12 @@
+// *** FLUTTER INTRODUCTION (ASSIGNMENT 1) (MODULE 1) ***
+void main()
+{
+  //  Question(01) Display this information using print your(name,bod,age and address)
+
+  print("Hello :)");
+  print("My name is barot bhavya brijeshkumar");
+  print("My birth date is (01,11,2000)");
+  print("I am 22 year old");
+  print("My address is (C1-fortune empire borisna road near ambika highway kalol,gujart 382721)");
+          
+}
